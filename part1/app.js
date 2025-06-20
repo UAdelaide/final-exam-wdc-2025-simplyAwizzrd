0b13b92ebedbce5509e9112f50fb4feecc22f72a
simplyAwizzrd <a1788462@adelaide.edu.au>
2025-06-20 01:37:38 +0000

    [automatic commit] 2025-06-20 01:37:38 UTC 45.249.116.246

--- part1/app.js
+++ part1/app.js
@@ -45,5 +45,6 @@ app.get('/api/dogs', async (req, res) => {
     try {
         const [rows] = await db.execute(`
             SELECT d.name AS dog_name, d.size, u.username AS owner_username
-            FROM `)
+            FROM Dogs d
+            JOIN Users u ON d.owner_id = u.user_id`)
     }
