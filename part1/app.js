37f51f7aee2b4f2f535afb6c0afbb47e927d09e6
simplyAwizzrd <a1788462@adelaide.edu.au>
2025-06-20 02:01:47 +0000

    [automatic commit] 2025-06-20 02:01:47 UTC 45.249.116.246

--- part1/app.js
+++ part1/app.js
@@ -75,6 +75,6 @@ app.get('/api/walkers/summary', async (req, res) => {
             SELECT u.username AS walker_username,
             COUNT(r.rating_id) AS total_ratings,
             ROUND(AVG(r.rating), 1) AS average_rating,
-            
+            SUM(CASE WHEN wr.status = 'completed')
 
             )
