76fa8ed60e4fbfd1d3b50be01b845a99cdd84296
simplyAwizzrd <a1788462@adelaide.edu.au>
2025-06-20 04:48:27 +0000

    [automatic commit] 2025-06-20 04:48:27 UTC 45.249.116.246

--- part2/app.js
+++ part2/app.js
@@ -30,7 +30,7 @@ mysql.createConnection({
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
 
-    const [rows] = await db.execute('SELECT * FROM Users WHERE username = ? AND password_hash = ?')
+    const [rows] = await db.execute('SELECT * FROM Users WHERE username = ? AND password_hash = ?', [username, password]);
 
 })
 
