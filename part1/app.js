35840caff913cdd0c31e43849e8d767026dc5183
simplyAwizzrd <a1788462@adelaide.edu.au>
2025-06-20 01:25:24 +0000

    [automatic commit] 2025-06-20 01:25:24 UTC 45.249.116.246

--- part1/app.js
+++ part1/app.js
@@ -18,5 +18,5 @@ async function testData() {
         INSERT IGNORE INTO Users (username, email, password_hash, role) VALUES
         ('alice123', 'alice@example.com', 'hashed123', 'owner'),
         ('bobwalker', 'bob@example.com', 'hashed456', 'walker');
-        ('carol123',)
+        ('carol123', 'carol@example.com', 'hashed7
 }
