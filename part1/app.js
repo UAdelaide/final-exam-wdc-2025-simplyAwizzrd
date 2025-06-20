83e7e09b46f163156534cb51eed0778b41719ee0
simplyAwizzrd <a1788462@adelaide.edu.au>
2025-06-20 01:46:49 +0000

    [automatic commit] 2025-06-20 01:46:49 UTC 45.249.116.246

--- part1/app.js
+++ part1/app.js
@@ -50,5 +50,5 @@ app.get('/api/dogs', async (req, res) => {
         `);
         res.json(rows);
     } catch (error) {
-        res.status(500).json({})
+        res.status(500).json({ error: })
     }
