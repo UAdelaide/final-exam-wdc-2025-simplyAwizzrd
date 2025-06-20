916ca0235aed780fd3b64908b2d3acee93f64b77
simplyAwizzrd <a1788462@adelaide.edu.au>
2025-06-20 04:45:43 +0000

    [automatic commit] 2025-06-20 04:45:43 UTC 45.249.116.246

--- part2/app.js
+++ part2/app.js
@@ -10,6 +10,10 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
 
+// Connect to the database
+let db;
+
+
 // Routes
 // Login route
 app.post('/login', async (req, res) => {
