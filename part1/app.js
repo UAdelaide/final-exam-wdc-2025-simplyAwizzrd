20ed13a60f51a1674d5603216efdf5dcf5055d4c
simplyAwizzrd <a1788462@adelaide.edu.au>
2025-06-20 02:16:04 +0000

    [automatic commit] 2025-06-20 02:16:04 UTC 45.249.116.246

--- part1/app.js
+++ part1/app.js
@@ -57,7 +57,7 @@ app.get('/api/dogs', async (req, res) => {
 app.get('/api/walkrequests/open', async (req, res) => {
     try {
         const [rows] = await db.execute(`
-            SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr_duration_minutes, wr.location, u.username AS owner_username
+            SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username
             FROM WalkRequests wr
             JOIN Dogs d ON wr.dog_id = d.dog_id
             JOIN Users u ON d.owner_id = u.user_id
