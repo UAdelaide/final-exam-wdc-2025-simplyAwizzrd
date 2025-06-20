030917dd2fdb3555c26e8ee2a6311bf645bc0d73
simplyAwizzrd <a1788462@adelaide.edu.au>
2025-06-20 01:55:40 +0000

    [automatic commit] 2025-06-20 01:55:40 UTC 45.249.116.246

--- part1/app.js
+++ part1/app.js
@@ -72,4 +72,5 @@ app.get('/api/walkrequests/open', async (req, res) => {
 app.get('/api/walkers/summary', async (req, res) => {
     try {
         const [rows] = await db.execute(`
-            SELECT 
+            SELECT u.username AS walker_username,
+            
