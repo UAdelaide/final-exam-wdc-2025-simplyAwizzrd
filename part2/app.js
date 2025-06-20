cdafe857a0fba8d13a363b177a4ac9cd1fb4b0a4
simplyAwizzrd <a1788462@adelaide.edu.au>
2025-06-20 04:40:17 +0000

    [automatic commit] 2025-06-20 04:40:17 UTC 45.249.116.246

--- part2/app.js
+++ part2/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
-const mysql = require('mysql2/promise')
+const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const app = express();
