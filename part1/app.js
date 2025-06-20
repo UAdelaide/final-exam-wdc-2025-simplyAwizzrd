bac0981885390d084707aa00e9404e3afe68039d
simplyAwizzrd <a1788462@adelaide.edu.au>
2025-06-20 01:31:31 +0000

    [automatic commit] 2025-06-20 01:31:31 UTC 45.249.116.246

--- part1/app.js
+++ part1/app.js
@@ -30,6 +30,7 @@ async function testData() {
     await db.execute(`
         INSERT IGNORE INTO Walks (request_id, walker_id, owner_id, rating) VALUES
         ((SELECT request_id FROM WalkRequests WHERE request_id = 2),
-        (SELECT user_id FROM Users WHERE username = )
+        (SELECT user_id FROM Users WHERE username = 'bobwalker'),
+        (SELECT user_id FROM Users WHERE username = 'carol123'), 5);
     `);
 }
