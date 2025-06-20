66574fa9f6c3da4384ccb0ccbf703a9a8f5ce1ea
simplyAwizzrd <a1788462@adelaide.edu.au>
2025-06-20 01:49:54 +0000

    [automatic commit] 2025-06-20 01:49:54 UTC 45.249.116.246

--- part1/app.js
+++ part1/app.js
@@ -57,5 +57,5 @@ app.get('/api/dogs', async (req, res) => {
 app.get('/api/walkrequests/open', async (req, res) => {
     try {
         const [rows] = await db.execute(`
-            SELECT wr.request_id, d.name AS dog_name, 
+            SELECT wr.request_id, d.name AS dog_name, wr.requested_time
     })
